1c124d4a81af56a3e0a12eb4ec8798d529f4ddc8
PEDRO-APU <62312971+PEDRO-APU@users.noreply.github.com>
2020-03-25 22:28:00 -0300

    Subo archivo

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjListas
+{
+    class Program
+    {
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+        static void Main(string[] args)
+        {
+            //punto a
+            List<String> listaColores = new List<string>(colors);
+            List<String> listaRemove = new List<string>(removeColors);
+
+         //punto b
+          foreach(String colores in listaColores)
+            {
+                Console.WriteLine(colores);
+            }
+
+            //punto c
+            listaColores = listaColores.Except(listaRemove).ToList();
+          
+
+            //punto d
+            Console.WriteLine("nueva lista");
+            foreach (String colores in listaColores)
+            {
+                Console.WriteLine(colores);
+            }
+        
+            Console.ReadKey();        
+
+            
+           
+        }
+    }
+        
+
+
+            
+        
+}
